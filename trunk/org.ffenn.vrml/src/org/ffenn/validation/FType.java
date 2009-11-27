83977014458a6814382b1e895b10a5b0e5f5f63c
hagbonon <hagbonon@bdb1fe7a-d9b2-11de-aa1f-bbd297b2055c>
2009-11-27 10:18:06 +0000

    Removed french, edited comments

--- trunk/org.ffenn.vrml/src/org/ffenn/validation/FType.java
+++ trunk/org.ffenn.vrml/src/org/ffenn/validation/FType.java
@@ -1,10 +1,10 @@
 package org.ffenn.validation;
 
 /**
- * Enum that list the different types that can a Field can take in VRML
- * Take note that MFBool is in a slighty different case : it's not part of VRML 97 specification
+ * Enum that lists the different types that a Field can take in VRML
+ * Please notice that MFBool is in a slighty different case : it's not part of VRML 97 specification
  * but as it can be used in most of VRML plugins and as SFBool would be the only type without a multi-side,
- * we considered that it was worth having it (and we can't possiblity let the poor SFBool be alone)
+ * we considered that it was worth having it (and we can't possibly let the poor SFBool be alone)
  *
  */
 public enum FType {
